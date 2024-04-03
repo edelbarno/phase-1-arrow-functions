0b3968255b2ae83b9fc8d5656a5846a3e411bd95
edelbarno <barnoedel12@gmail.com>
2024-04-03 15:50:37 +0300

    add arrow lab

---
+++ index.js
@@ -0,0 +1,17 @@
+function divide(a,b) {
+    return a/b;
+}
+let a = 2000;
+let b =100;
+console.log(divide(a, b))
+
+const add = (a,b) => {
+
+    return a+b;
+}
+console.log(add(3,4));
+const square = (a) => {
+  return a*a;
+
+}
+console.log(square(a))
